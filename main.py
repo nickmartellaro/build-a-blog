53e64b568e7fc166849cb70504984305e13ce94e
Nicholas Martellaro <Nicholas Martellaro>
2017-02-08 20:34:00 -0600

    trying to get blog hanlder to work showing error

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class Blog(db.Model):
+
+    blog_id = db.StringProperty(required = True)
+    title = db.StringProperty(required = True)
+    blog_text = db.TextProperty(required = True)
+    ##created = db.DateTimeProperty(auto_now_add=True)
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class MainPage(Handler):
     def get(self):
         self.response.write('Hello world!')
 
+    # def get(self):
+    #     self.render("blog.html")
+
+class BlogPage(Handler):
+    def get(self):
+        text = "test"
+        t = jinja_env.get_template("blog.html")
+        content = t.render(text = text)
+        self.render(content)
+
+class NewPost(Handler):
+    def get(self):
+        self.render("newpost.html")
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage),
+    ('/blog', BlogPage),
+    ('/newpost', NewPost)
 ], debug=True)
